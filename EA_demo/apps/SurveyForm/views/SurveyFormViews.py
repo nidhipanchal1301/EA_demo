c83de8c5ed280cebfecbff7b45368973d73e0ff8
nidhipanchal1301 <nidhi.web.moweb@gmail.com>
2025-12-31 18:40:12 +0530

    Refector : Added SurveyForm API with nested serializers and validations did correction in serializer and in create api

--- EA_demo/apps/SurveyForm/views/SurveyFormViews.py
+++ EA_demo/apps/SurveyForm/views/SurveyFormViews.py
@@ -7,44 +7,57 @@ from rest_framework.permissions import AllowAny
 from ..models import SurveyForm, SurveyQuestion, SurveyOption, SurveyPermission
 
 from ..serializers.SurveyFormSerializers import (SurveyFormCreateSerializer, SurveyFormListSerializer,
-                                                SurveyFormDetailSerializer, SurveyFormResponseSerializer,)
-
+                                                SurveyFormDetailSerializer, SurveyFormResponseSerializer, 
+                                                SurveyFormUpdateSerializer,)
+                                                
 
 
 class SurveyFormCreateAPIView(CreateAPIView):
+    queryset = SurveyForm.objects.all().prefetch_related("permissions", "SurveyQuestion_form__SurveyOption_question")
     permission_classes = [AllowAny]
     serializer_class = SurveyFormCreateSerializer
-    queryset = SurveyForm.objects.all()
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         data = serializer.validated_data
-        form = SurveyForm.objects.create(name=data["name"], status=data["status"])
+        instance = SurveyForm.objects.create(name=data["name"],status=data.get("status", "active"))
         permissions_qs = SurveyPermission.objects.filter(id__in=data["permissions"])
-        form.permissions.set(permissions_qs)
+        instance.permissions.set(permissions_qs)
 
-        for q in data["questions"]:
-            question = SurveyQuestion.objects.create(
-                form=form,
+        questions_to_create = [
+            SurveyQuestion(
+                form=instance,
                 label=q.get("label"),
                 placeholder=q.get("placeholder", ""),
                 type=q.get("type"),
                 mandatory=q.get("mandatory", False),
                 default_hide=q.get("default_hide", False),
-                use_for_analytics=q.get("use_for_analytics", False),)
+                use_for_analytics=q.get("use_for_analytics", False),
+            )
+            for q in data["questions"]
+        ]
 
-            SurveyOption.objects.bulk_create([
-                SurveyOption(
-                    question=question,
-                    text=opt.get("text") if isinstance(opt, dict) else opt
-                )
-                for opt in q.get("options", [])
-            ])
+        created_questions = SurveyQuestion.objects.bulk_create(questions_to_create)
 
-        response_serializer = SurveyFormResponseSerializer(form, context={"request": request})
+        options_to_create = [
+            SurveyOption(
+                question=created_q,
+                text=(opt.get("text") if isinstance(opt, dict) else opt)
+            )
+            for created_q, original_q in zip(created_questions, data["questions"])
+            for opt in original_q.get("options", [])
+            if original_q.get("type") in ["single_choice", "multi_choice"]
+        ]
 
+
+        if options_to_create:
+            SurveyOption.objects.bulk_create(options_to_create)
+
+        response_serializer = SurveyFormResponseSerializer(instance)
         return Response(response_serializer.data, status=status.HTTP_201_CREATED)
+
+
 
     
 class SurveyFormListAPIView(ListAPIView):
@@ -55,15 +68,19 @@ class SurveyFormListAPIView(ListAPIView):
     def get_queryset(self):
         queryset = super().get_queryset()
         search = self.request.query_params.get("search")    
+
         if search:
-            queryset = queryset.filter(name=search)
+            queryset = queryset.filter(
+            Q(name__icontains=search) |
+            Q(permissions__key__icontains=search)
+        ).distinct()
         return queryset
 
 
 class SurveyFormUpdateAPIView(UpdateAPIView):
     queryset = SurveyForm.objects.all().prefetch_related("permissions", "SurveyQuestion_form__SurveyOption_question")
     permission_classes = [AllowAny]
-    serializer_class = serializers.Serializer
+    serializer_class = SurveyFormUpdateSerializer
 
     def update(self, request, *args, **kwargs):
         instance = self.get_object()
@@ -90,19 +107,16 @@ class SurveyFormUpdateAPIView(UpdateAPIView):
             ]
             
             created_questions = SurveyQuestion.objects.bulk_create(questions_to_create)
-            options_to_create = []
-            for question, q_data in zip(created_questions, data["questions"]):
-                opts = [
-                    SurveyOption(
-                        question=question,
-                        text=opt.get("text") if isinstance(opt, dict) else opt
-                    )
-                    for opt in q_data.get("options", [])
-                ]
-                options_to_create.extend(opts)
+
+            options_to_create = [
+                SurveyOption(question=question, text=(opt.get("text")if isinstance(opt, dict)else opt))
+                for question, q_data in zip(created_questions, data["questions"])
+                for opt in q_data.get("options", [])
+            ]
+
             SurveyOption.objects.bulk_create(options_to_create)
 
-        response_serializer = SurveyFormResponseSerializer(instance, context={"request": request})
+        response_serializer = SurveyFormResponseSerializer(instance)
         return Response(response_serializer.data, status=status.HTTP_200_OK)
     
 
